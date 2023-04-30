0a27d31cce8ef8496b0d31790a7269129d560489
ayoung4 <alfredsyoung@gmail.com>
2023-04-30 12:18:02 -0700

    add to code comments

--- src/main.ts
+++ src/main.ts
@@ -12,15 +12,22 @@ import { NewsletterService } from './apps/newsletter.js'
 import { WebAPIService } from './apps/web-api.js'
 import { WebUIService } from './apps/web-ui.js'
 
+// The main function demonstrates how to wire up
+// several independent services to an event bus
+// Read more about event driven architecture:
+// https://learn.microsoft.com/en-us/azure/architecture/guide/architecture-styles/event-driven
+
 function main() {
+  // Create event bus
   const eventBus = Observable<TodoEvent>([])
-
+  // Create projections
   const todosDb = TodosTableProjection({})
-  const latestTodos = LatestTodoTitlesProjection([])
-
+  const titlesCache = LatestTodoTitlesProjection([])
+  // Wire projections up to event bus
   eventBus.addListener(todosDb.processEvent)
-  eventBus.addListener(latestTodos.processEvent)
-
+  eventBus.addListener(titlesCache.processEvent)
+  // Create some mock events to simulate user
+  // interactions
   const mockEvents = [
     TodoAdded({ id: '0', title: 'learn fp' }),
     TodoAdded({ id: '1', title: 'separate reads and writes' }),
@@ -31,12 +38,13 @@ function main() {
     TodoRemoved({ id: '3' }),
     TodoDone({ id: '2' }),
   ]
-
+  // Create some services and inject appropriate dependencies
   const webAPI = new WebAPIService(mockEvents, eventBus)
   const webUI = new WebUIService(todosDb)
-  const newsletter = new NewsletterService(latestTodos)
-
+  const newsletter = new NewsletterService(titlesCache)
+  // Run all services in parallel
   return Promise.all([webUI.run(), webAPI.run(), newsletter.run()])
 }
 
+// Dont forget to run main!
 main()
